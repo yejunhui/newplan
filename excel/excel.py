c53c4fb5efc0293ae0a5fbd676e8c95555ec4d5d
yejunhui <1589082159@qq.com>
2020-05-20 20:33:16 +0800

    1

---
+++ excel/excel.py
@@ -0,0 +1,45 @@
+import xlrd ,openpyxl
+import os
+
+#Excel文件处理类
+class excel:
+    def __init__(self,readFile,writeFile):
+        self.thisFile = os.getcwd()
+        self.readFile = readFile
+        self.wriFile = writeFile
+
+    #读取Excel
+    def readExcel(self):
+        try:
+            #Excel数据
+            excelData = {}
+            #Excel表数据
+            excelSheetData = []
+            data = []
+            #openFile
+            e = xlrd.open_workbook(self.readFile)
+            #获取工作表数量
+            sheetQuantity = len(e.sheets())
+            sheeetNames = e.sheet_names()
+            #读取表内容
+            for n in sheeetNames:
+                #读取表
+                s = e.sheet_by_name(n)
+                #获得总行
+                nrows = s.nrows
+                for j in range(nrows):
+                    data.append(s.row(j))
+                excelSheetData.append(data)
+                data = []
+                excelData[n] = excelSheetData
+                excelSheetData = []
+
+            return excelData
+
+
+        except:
+            print('There seems to be a problem!--> -->')
+
+    #写入Excel
+    def writeExcel(self):
+        pass
